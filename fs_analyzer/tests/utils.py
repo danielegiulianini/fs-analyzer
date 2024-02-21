c301103caf4efdf616a068c4556bd98629b54ec9
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2024-02-21 19:18:25 +0100

    remove useless test utils

--- fs_analyzer/tests/utils.py
+++ fs_analyzer/tests/utils.py
@@ -49,7 +49,6 @@ class DirectoryTreeScenario:
         self._files_paths = []
         self._test_path = test_path
 
-    #called once for all the functions in the module
     def create_directory_tree(self):
         
         print("setting up directory tree...")
@@ -70,51 +69,6 @@ class DirectoryTreeScenario:
         os.mkdir(join(self._test_path, 'emptydir'))
         
 
-    #called once for all the functions in the module
-    def remove_directory_tree(self):
-        print("setting down directory tree.")
-        try:
-            shutil.rmtree(self._test_path)
-        except OSError:
-            time.sleep(0.1)
-            shutil.rmtree(self._test_path)
-            
-    def test_path(self):
-        return self._test_path
-    
-    def files_paths(self):
-        return self._files_paths
-
-
-
-class EmptyDirectoryTreeScenario:
-
-    def __init__(self, test_path):
-        self._files_paths = []
-        self._test_path = test_path
-
-    #called once for all the functions in the module
-    def create_directory_tree(self):
-        
-        print("setting up directory tree")
-        join = os.path.join
-
-        os.mkdir(self._test_path)
-        os.mkdir(join(self._test_path, 'subdir'))
-        
-        self._files_paths.append(create_file(join(self._test_path, 'file1.txt')))
-        self._files_paths.append(create_file(join(self._test_path, 'file2.txt'), contents='12345678'))
-
-        os.mkdir(join(self._test_path, 'subdir', 'unidir\u018F'))
-        self._files_paths.append(create_file(join(self._test_path, 'subdir', 'file1.txt'), contents="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbb"))
-        self._files_paths.append(create_file(join(self._test_path, 'subdir', 'unicod\u018F.py')))
-
-        self._files_paths.append(create_file(join(self._test_path, 'subdir', 'unidir\u018F', 'file1.txt')))
-
-        os.mkdir(join(self._test_path, 'linkdir'))
-        
-
-    #called once for all the functions in the module
     def remove_directory_tree(self):
         print("setting down directory tree.")
         try:
