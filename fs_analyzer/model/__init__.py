c7f5a71a6bc845dc95cb81347265a60896e2f875
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2024-02-21 18:46:59 +0100

    fix typo in doc

--- fs_analyzer/model/__init__.py
+++ fs_analyzer/model/__init__.py
@@ -5,17 +5,21 @@ and report on the file system structure and usage, designed for handling
 large directories. They are reusable outside of the scope of the fs_analyzer 
 command line application app.
     
-In particular:
+In particular,
 
 Modules:
-* file_categorization_strategy.py: containing some interchangeable strategies for classifying
-    files, modeled through the so-called "Strategy" Object-Oriented (OO) design pattern.
-* file_category: containing the definition of the class representing the abstraction of file category.
-* file_listing_generators: containing many reusable and performant generators of information 
-    like size, category and permissions regarding the file of a specified directory tree, suitable 
-    for exploring large directories.
-* file_permission_reporting_strategy: containing some interchangeable strategies for identfying files
-    with unusual permission settings, modeled through the so-called "Strategy" OO design pattern.
-* file_permissions: containing the definition of the class representing the abstraction of file permission.
+* file_categorization_strategy.py: containing some interchangeable strategies for 
+    classifying files, modeled through the so-called "Strategy" Object-Oriented
+    (OO) design pattern.
+* file_category: containing the definition of the class representing the abstraction
+    of file category.
+* file_listing_generators: containing many reusable and performant generators of
+    information like size, category and permissions regarding the file of a 
+    specified directory tree, suitable for exploring large directories.
+* file_permission_reporting_strategy: containing some interchangeable strategies for 
+    identifying files with unusual permission settings, modeled through the so-called 
+    "Strategy" OO design pattern.
+* file_permissions: containing the definition of the class representing the abstraction 
+    of file permission.
 
 """
