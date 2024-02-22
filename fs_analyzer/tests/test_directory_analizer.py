7d1a030d3c918c41174ed7e7b9584d6d2ff8cf5e
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2024-02-22 11:58:55 +0100

    refactor test_directory_analyzer.py to reduce repeated code

--- fs_analyzer/tests/test_directory_analizer.py
+++ fs_analyzer/tests/test_directory_analizer.py
@@ -7,8 +7,12 @@ from fs_analyzer.view.cli_view import *
 from fs_analyzer.view_model.directory_analizer_factory import ExtensionDirectoryAnalizerFactory
 
 
+# test fixtures
 TEST_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'testdir'))
 directory_tree = DirectoryTreeScenario(TEST_PATH)
+directory_analizer_factory = ExtensionDirectoryAnalizerFactory()
+directory_analizer = directory_analizer_factory.create(directory_path = directory_tree.test_path(),
+                                                       directory_observer = MagicMock())
 
 # could use the ad-hoc fixture https://docs.pytest.org/en/6.2.x/tmpdir.html 
 # to raise the abstraction level and avoid boilerplate
@@ -21,19 +25,17 @@ def teardown_module():
 
 def test_handle_provided_directory_not_found_gracefully():
     mock = MagicMock()
-    directory_analizer = ExtensionDirectoryAnalizerFactory().create(directory_path = directory_tree.test_path() + "jsj", 
-                                                                    directory_observer = mock) 
+    not_existing_directory_analizer = directory_analizer_factory.create(directory_path = directory_tree.test_path() + "jsj", 
+                                                                        directory_observer = mock) 
     try:  
-        directory_analizer.categorize_files()
+        not_existing_directory_analizer.categorize_files()
     except Exception as excinfo:  
-        fail(f"Unexpected exception raised: {excinfo}") 
+        fail(f"Unexpected exception raised: {excinfo}")
 
     mock.on_invalid_input.assert_called_once_with("The provided path does not point to a directory.")
 
 
 def test_handle_provided_directory_not_readable_gracefully():
-    directory_analizer = ExtensionDirectoryAnalizerFactory().create(directory_path = directory_tree.test_path(), 
-                                                                    directory_observer = MagicMock())
     
     # remove permission to open a directory
     no_read_permission_mode = 0o444
@@ -51,10 +53,8 @@ def test_handle_provided_directory_not_readable_gracefully():
 
 @patch('fs_analyzer.view_model.directory_analizer.yield_file_categories')
 def test_handle_file_not_found_during_tree_traversal_gracefully(test_patch):
-    test_patch.side_effect = FileNotFoundError('mocked error')
     
-    directory_analizer = ExtensionDirectoryAnalizerFactory().create(directory_path = directory_tree.test_path(), 
-                                                                    directory_observer = MagicMock())
+    test_patch.side_effect = FileNotFoundError('mocked error')
     
     try:  
         directory_analizer.categorize_files()
